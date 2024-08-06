a59391f4c6e11168c3a310005952cfae14c173e5
Hrpl <ponomarevd61@gmail.com>
2024-08-06 19:48:31 +0300

    Add config for JWT

--- SearchRepository.API/Program.cs
+++ SearchRepository.API/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.IdentityModel.Tokens;
 using SearchRepository.Application.Interface;
 using SearchRepository.Application.Services;
 using SearchRepository.Infrastructure;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var configuration = builder.Configuration;
+
+var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:SecretKey"]));
 
 // Add services to the container.
 
@@ -15,6 +23,21 @@ builder.Services.AddAntiforgery();
 
 builder.Services.AddDbContext<SearchRepositoryDBContext>();
 builder.Services.AddScoped<ISearchRepository, SearchServices>();
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = configuration["Jwt:Issuer"],
+            ValidateAudience = true,
+            ValidAudience = configuration["Jwt:Audience"],
+            ValidateLifetime = true,
+            IssuerSigningKey = key
+        };
+    });
 
 var app = builder.Build();
 
