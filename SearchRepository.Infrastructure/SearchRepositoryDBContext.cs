57fc01ab5edb94e552513bf57ab7bf015af81656
DenisPonomarev22 <ponomarevd200522@gmail.com>
2024-08-06 03:46:46 +0300

    Добавил изменения к подключению к бд

--- SearchRepository.Infrastructure/SearchRepositoryDBContext.cs
+++ SearchRepository.Infrastructure/SearchRepositoryDBContext.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 using SearchRepository.Domen;
 using SearchRepository.Domen.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace SearchRepository.Infrastructure;
 
@@ -15,12 +16,22 @@ public class SearchRepositoryDBContext : DbContext
 
     public DbSet<SearchRequest> SearchRequests { get; set; }
 
-    public SearchRepositoryDBContext()
+    public SearchRepositoryDBContext(DbContextOptions<SearchRepositoryDBContext> opt) : base(opt)
     {
         Database.Migrate();
     }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseNpgsql("Host=localhost;Port=5435;Database=postgres;Username=postgres;Password=12345");
+        optionsBuilder.UseNpgsql("Host=127.0.0.1;Port=5435;Database=postgres;Username=postgres;Password=12345;Pooling=true;");
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        //modelBuilder.Entity<Repository>(ConfigureRepository);
+    }
+    /*private static void ConfigureRepository(EntityTypeBuilder<Repository> builder)
+    {
+        builder.Property(x => x.Name).IsRequired();
+    }*/
 }
